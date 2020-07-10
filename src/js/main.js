88e77c3cc1f2c850f8f82af64d048eee1b274ad6
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-07-10 22:27:03 +0300

    added rework

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,52 @@ $(function(){
 
   var mask = IMask(element, maskOptions);
 
+  $('.menu__btn').on('click', function(){
+    this.classList.toggle('active')
+    $('.menu__list').slideToggle();
+  });
+
+  AOS.init({
+    delay: 300,
+    duration: 1000,
+  });
+
+
+});
+
+function scrollTo(element) {
+  window.scroll({
+    left: 0,
+    top: element.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+var home = document.querySelector('.home');
+var header = document.querySelector('.header');
+
+var specifications = document.querySelector('.specifications');
+var description = document.querySelector('.description');
+
+var form = document.querySelector('.form');
+var purchase = document.querySelector('.purchase');
+
+var contacts = document.querySelector('.contacts');
+var footer = document.querySelector('.footer');
+
+
+home.addEventListener('click', () => {
+  scrollTo(header);
+});
+
+specifications.addEventListener('click', () => {
+  scrollTo(description);
+});
+
+form.addEventListener('click', () => {
+  scrollTo(purchase);
+});
+
+contacts.addEventListener('click', () => {
+  scrollTo(footer);
 });
